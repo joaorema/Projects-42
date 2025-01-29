6498e840691fde6ba08ff3e40f21d85118d81a23
joaorema <joaopcrema@gmail.com>
2025-01-29 16:36:58 +0000

    changes to gnl

--- Exames/ExameRank2/grademe_sucess/get_next_line/get_next_line.c
+++ Exames/ExameRank2/grademe_sucess/get_next_line/get_next_line.c
@@ -1,63 +1,89 @@
-# include <stdlib.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdio.h> //for testing main
+#include "get_next_line.h"
 
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-# endif
+static char *append_char(char *str, char c, int len)
+{
+    char *new_str;
+    int i;
+
+    new_str = (char *)malloc(len + 2); // +1 for new char, +1 for '\0'
+    if (!new_str)
+        return (NULL);
+    i = 0;
+    while (i < len)
+    {
+        new_str[i] = str[i];
+        i++;
+    }
+    new_str[i++] = c;
+    new_str[i] = '\0';
+    free(str);
+    return (new_str);
+}
 
 char *get_next_line(int fd)
 {
-    int     byte;
-    char    c;
-    char    *str_buffer;
-    int     i;
+    static char buffer[BUFFER_SIZE];
+    static int buf_index = 0;
+    static int buf_size = 0;
+    char *line = NULL;
+    int i = 0;
 
     if (BUFFER_SIZE < 1 || fd < 0)
         return (NULL);
-    i = 0;
-    str_buffer = (char *)malloc(10000000);
-    if (!str_buffer)
-    {
-        free(str_buffer);
+    
+    line = (char *)malloc(1);
+    if (!line)
         return (NULL);
-    }
-    byte = read(fd, &c, 1); 
-    while (byte > 0)
-    {
-        str_buffer[i] = c;
-        i++;
-        if(c == '\n' || c == '\0')
-            break ;
-        byte = read(fd, &c, 1); 
-    }
-    if (i == 0 || byte < 0)
-    {
-        free(str_buffer);
-        return (NULL);
-    }
-    str_buffer[i] = '\0';
-    return (str_buffer);
-}
+    line[0] = '\0';
 
+    while (1)
+    {
+        if (buf_index >= buf_size)
+        {
+            buf_size = read(fd, buffer, BUFFER_SIZE);
+            buf_index = 0;
+            if (buf_size <= 0)
+            {
+                if (i == 0)
+                {
+                    free(line);
+                    return (NULL);
+                }
+                break;
+            }
+        }
+        line = append_char(line, buffer[buf_index++], i);
+        if (!line)
+            return (NULL);
+        if (line[i++] == '\n')
+            break;
+    }
+    return (line);
+}
+/*
 int main(void)
 {
     int     fd;
     char    *str;
     char    *path;
-    int     i;
+    int     line;
 
     path = "test.txt";
     fd = open(path, O_RDONLY);
-    i = 0;
-    while(i < 10) // number of loop (test)
+    if (fd < 0)
     {
-        str = get_next_line(fd);
-        free(str);
-        printf("i: %i\n", i);
-        printf("fd: %i, %s\n", fd, str);
-        i++;
+        perror("Error opening file");
+        return (1);
     }
+    line = 1;
+    while ((str = get_next_line(fd)))
+    {
+        printf("Linha: %i\n", line);
+        printf("%s\n", str); 
+        free(str); 
+        line++;
+    }
+    close(fd); 
     return (0);
 }
+*/
