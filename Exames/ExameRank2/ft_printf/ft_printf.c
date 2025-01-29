33953554603f340e32ecd9fdd39819b76cf2ee85
joaorema <joaopcrema@gmail.com>
2025-01-29 14:48:46 +0000

    exam2 gnl

--- Exames/ExameRank2/ft_printf/ft_printf.c
+++ Exames/ExameRank2/ft_printf/ft_printf.c
@@ -1,39 +1,42 @@
 #include <unistd.h>
 #include <stdarg.h>
-#include <stdio.h>  // for testing return values
 
-void ft_putstr(char *str, int *len)
+void    ft_putstr(char *str, int *len)
 {
     if(!str)
         str = "(null)";
     while(*str)
         *len = *len + write(1, str++, 1);
 }
-void ft_putnbr(int nb, int base, int *len)
+
+void    ft_putnbr(int nb, int base, int *len)
 {
-    char *hex = "0123456789abcedf";
+    char *hex = "0123456789abcdef";
 
     if(nb < 0)
     {
-        write(1, "-", 1);
-        len++;
+        *len = *len + write(1, "-", 1);
         nb = -nb;
+    }
+    if(nb == -2147483648)
+    {
+        *len = *len + write(1, "2147483648", 10);
     }
     if(nb >= base)
         ft_putnbr((nb / base), base, len);
-    *len = *len + write(1, &hex[nb % base], 1);
+    *len = *len + write(1, &hex[(nb % base)], 1);
 }
 
-int ft_printf(const char *type, ...)
+int     ft_printf(const char *type, ...)
 {
-    va_list args;
     int i = 0;
     int len = 0;
+    va_list(args);
 
     va_start(args, type);
     while(type[i])
     {
-        if((type[i] == '%') && (type[i + 1] != '\0'))
+        if((type[i] == '%') && (type[i+1] != '\0'))
         {
             i++;
             if(type[i] == 's')
@@ -44,9 +47,7 @@ int ft_printf(const char *type, ...)
                 ft_putnbr(va_arg(args, int), 16, &len);
         }
         else
-        {
             len = len + write(1, &type[i], 1);
-        }
         i++;
     }
     va_end(args);
@@ -55,30 +56,7 @@ int ft_printf(const char *type, ...)
 
 int main()
 {
-    char *teste = NULL;
-    char *teste1 = "lets test len!";
-    int og = 0;
-    int mine = 0;
-
-    ft_printf("1* Test:\n");
-    ft_printf("%s\n", "toto");
-    ft_printf("\n");
-
-    ft_printf("2* Test:\n");
-    ft_printf("Magic %s is %d\n", "number", 2);
-    ft_printf("\n");
-
-    ft_printf("3* Test:\n");
-    ft_printf("Hexadecimal for %d is %x\n", 42, 42);
-    ft_printf("\n");
-
-    ft_printf("4* Test:\n");
-    ft_printf("A Null string : %s\n", teste);
-    ft_printf("\n");
-
-    mine = ft_printf("5* Test: %s\n", teste1);
-    og = printf("5* Test: %s\n", teste1);
-    ft_printf("The len of my ft: %d\n", mine);
-    ft_printf("The len of orignal: %d\n", og);
-
+    ft_printf("string test : %s\n", "lets go!");
+    ft_printf("decimal test : %d\n", -42);
+    ft_printf("hexa test of -42 : %x\n", -42);
 }
---
+++ Exames/ExameRank2/get_next_line/get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h> //for testing main
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+
+# endif
+#endif
+
